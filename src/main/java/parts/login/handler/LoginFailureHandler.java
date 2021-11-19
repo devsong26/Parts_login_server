cb8e5a0e88021c6990ddd86dbacec93c3d434eb0
devsong26 <devsong26@gmail.com>
2021-11-19 22:34:36 +0900

    로그인 실패 구현

--- src/main/java/parts/login/handler/LoginFailureHandler.java
+++ src/main/java/parts/login/handler/LoginFailureHandler.java
@@ -1,16 +1,33 @@
 package parts.login.handler;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import parts.login.component.builder.MessageBuilder;
+import parts.login.domain.Message;
+import parts.login.util.ResponseUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class LoginFailureHandler implements AuthenticationFailureHandler {
+
+    @Autowired
+    private MessageBuilder messageBuilder;
+
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-
+        try{
+            ResponseUtil.setResponseOption(response);
+            PrintWriter writer = ResponseUtil.getPrintWriter(response);
+            Message successMsg = messageBuilder.getMessage(request.getLocale(), HttpStatus.NOT_FOUND);
+            ResponseUtil.response(writer, successMsg);
+        }catch(IOException e){
+            e.getStackTrace();
+        }
     }
 }
